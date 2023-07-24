b0c7e36f5cc0515e56701c6bc820dfd9215971bd
allaeddineattia <attia.allaeddine@gmail.com>
2023-07-24 11:38:54 +0200

    chore: refactor the project structure

---
+++ src/game/xbox_api_client/input_dto/leaving_soon_response.rs
@@ -0,0 +1,28 @@
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct LeavingSoonResponse
+{
+    pub sigl_id: Option<String>,
+    pub id: Option<String>,
+    pub title: Option<String>,
+    pub description: Option<String>,
+    pub total_result_count: Option<u32>,
+    pub image_url:  Option<String>,
+}
